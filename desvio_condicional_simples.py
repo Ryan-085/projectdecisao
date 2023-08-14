30444f244249b07324bda7010b86c04a05bdb1b4
ART0059233 <ryanvirginio@gmail.com>
2023-08-14 11:50:22 -0300

    git

---
+++ desvio_condicional_simples.py
@@ -0,0 +1,14 @@
+'''
+crie um programa que pergunte dois números no usuário.
+Em seguida o programa deverá somar os dois números e
+apresentar o resultado se o valor for maior que 10
+'''
+
+num1 = int(input("informe um número: "))
+num2 = int(input("informe outro número: "))
+
+soma = num1 + num2
+
+if(some > 10 ):
+    print(f"A soma dos valores inseridos é {soma}" )
+    print("FIM DO PROGRAMA")
